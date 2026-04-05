59997c7ae33ac6df0112ff4adff28c633ffaec44
Elvis Nilsson <Nilssonelvis@gmail.com>
2026-04-05 17:21:16 +0200

    Validators

---
+++ src/BugArena.Application/Validators/UpdateChallengeValidator.cs
@@ -0,0 +1,36 @@
+﻿using BugArena.Application.DTOs.Challenges;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BugArena.Application.Validators
+{
+    public class UpdateChallengeValidator : AbstractValidator<UpdateChallengeRequest>
+    {
+        public UpdateChallengeValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Title is required.")
+                .MaximumLength(200);
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Description is required.")
+                .MaximumLength(5000);
+
+            RuleFor(x => x.ExpectedBehavior)
+                .NotEmpty().WithMessage("Expected behavior is required.")
+                .MaximumLength(2000);
+
+            RuleFor(x => x.ActualBehavior)
+                .NotEmpty().WithMessage("Actual behavior is required.")
+                .MaximumLength(2000);
+
+            RuleFor(x => x.Hints)
+                .MaximumLength(2000).When(x => x.Hints != null);
+
+            RuleFor(x => x.Difficulty)
+                .IsInEnum().WithMessage("Invalid difficulty.");
+        }
+    }
+}
