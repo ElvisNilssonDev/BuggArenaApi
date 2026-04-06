2ebed3be751177ff9126c6fe5bcc3f51132f10ee
Abdulkadir Hashi <dadirhashi544@gmail.com>
2026-04-06 16:18:29 +0200

    added lProfileRepository and LeaderboardRepository

---
+++ src/BugArena.Application/Interfaces/ILeaderboardRepository.cs
@@ -0,0 +1,16 @@
+﻿using BugArena.Application.DTOs.Leaderboard;
+
+namespace BugArena.Application.Interfaces;
+
+public interface ILeaderboardRepository
+{
+    Task<List<LeaderboardEntry>> GetGlobalTopAsync(int count = 50);
+    Task<List<LeaderboardEntry>> GetWeeklyTopAsync(int count = 50);
+    Task<int> GetGlobalRankAsync(Guid userId);
+    Task<int> GetWeeklyRankAsync(Guid userId);
+    Task<int> GetWeeklyPointsAsync(Guid userId);
+    Task<int> GetChallengesSolvedAsync(Guid userId);
+    Task<int> GetChallengesCreatedAsync(Guid userId);
+    Task<int> GetAverageSolveTimeAsync(Guid userId);
+    Task<string> GetFavoriteLanguageAsync(Guid userId);
+}
