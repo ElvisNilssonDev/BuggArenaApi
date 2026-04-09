333ecefadf879810b765c184853b4fa4ccc2e45b
Abdulkadir Hashi <dadirhashi544@gmail.com>
2026-04-09 21:56:17 +0200

    This tells the backend to accept "Easy" instead of 0, "JavaScript" instead of a number, etc. It works for all enums across your whole API.

--- src/BugArena.API/Program.cs
+++ src/BugArena.API/Program.cs
@@ -74,7 +74,12 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 
 builder.Services.AddAuthorization();
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(
+            new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 builder.Services.AddValidatorsFromAssemblyContaining<CreateChallengeValidator>();
 builder.Services.AddEndpointsApiExplorer();
 
